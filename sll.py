86e8f10644008d6d8019ef6b27e49bcc8fc35e20
prajwalshetty05 <132898699+prajwalshetty05@users.noreply.github.com>
2024-08-01 10:17:17 +0530

    single linked list

---
+++ sll.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class singlelinklist:
+    def __init__(self):
+        self.head=None
+    def append(self,data):
+        newnode=Node(data)
+        if self.head is None:
+            self.head=newnode
+            return
+        last=self.head
+        while last.next:
+            last=last.next
+        last.next=newnode
+    def display(self):
+        last=self.head
+        while(last):
+            print(last.data)
+            last=last.next
+    def push(self,data):
+        newnode=Node(data)
+        newnode.next=self.head
+        self.head=newnode
+    def removingnode(self,target):
+        prednode=None
+        currnode=self.head
+        while(currnode is not None and currnode.data!=target):
+            prednode=currnode
+            currnode=currnode.next
+            if(currnode is not None):
+                if(currnode is self.head):
+                    self.head=currnode.next
+                else:
+                    prednode.next=currnode.next
+    def search(self,x):
+        current=self.head
+        while(current!=None):
+            if(current.data==x):
+                print("found")
+                return
+            current=current.next
+        print("not found")
+    def insertafter(self,prev,insert):
+        newnode=Node(insert)
+        last=self.head
+        while(last and last.data!=prev):
+            last=last.next
+        newnode.next=last.next
+        last.next=newnode
+    
+
+l1=singlelinklist()
+l1.append(10)
+l1.append(20)
+l1.append(30)
+l1.append(40)
+l1.append(50)
+l1.display()
+print("push")
+l1.push(25)
+l1.display()
+print("removing")
+l1.removingnode(20)
+l1.display()
+print('insertafter')
+l1.insertafter(25,50)
+l1.display()
+print("search")
+l1.search(50)
+
